0e9d8e4391389880c5d8df0a294771b8dca60518
Ashis kumar Jena <ashis.jena96@gmail.com>
2019-07-06 20:03:22 +0530

    added template engine

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,25 @@ var User = require('./models/user');
 
 module.exports = function(app){
     app.get('/',(req,res) =>  {
-        res.send("Hello world")
+        res.render('index.ejs');
     })
+
+    app.get('/signup',(req,res) => {
+        res.render('signup.ejs',{message : 'Victory'});
+    })
+
+    app.post('/signup', (req, res)=>{
+		var newUser = new User();
+		newUser.local.username = req.body.email;
+		newUser.local.password = req.body.password;
+		newUser.save(function(err){
+			if(err)
+				throw err;
+		});
+
+		res.redirect('/');
+	});
+
 
     app.get('/:username/:password',(req,res) => {
         var newUser = new User();
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var configDB = require('./config/database');
 mongoose.connect(configDB.url,{useNewUrlParser:true});
@@ -18,6 +19,8 @@ app.use(session({
     saveUninitialized : true,
     resave : true
 }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
 
 /*
 app.use('/',(req,res) => {
