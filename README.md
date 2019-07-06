ac5d41b1ea2ae0aa5d786aaf3307651612a3453e
Ashis kumar Jena <ashis.jena96@gmail.com>
2019-07-06 17:49:09 +0530

    added cookie parser and express session

---
+++ README.md
@@ -0,0 +1,10 @@
+1. cookie-parser
+```
+cookie-parser looks at the header in between the client and server transaction.
+read those headers and parses out the cookie that are being send
+```
+
+2. express-session
+```
+It allows us to authenticate the transaction between the client and server
+```
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ var app = express();
 const port = process.env.PORT || 8080
 
 var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret : 'AnyString',
+    saveUninitialized : true,
+    resave : true
+}));
 
 app.use('/',(req,res) => {
-    res.send("Hello world")
-})
+    res.send("Hello world");
+    console.log(req.cookies);
+    console.log("============");
+    console.log(req.session);
+});
 
 app.listen(port);
-console.log("server staerte on port " + port);
+console.log("server started on port " + port);
