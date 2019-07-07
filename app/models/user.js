f022ad51b8b2062dd6dc4d9431c5e68977e64057
Ashis kumar Jena <ashis.jena96@gmail.com>
2019-07-07 10:07:36 +0530

    added encryption to password

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
     local : {
@@ -7,4 +8,11 @@ var userSchema = mongoose.Schema({
     }
 })
 
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password,bcrypt.genSaltSync(9));
+}
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.local.password);
+}
 module.exports = mongoose.model('User',userSchema);
